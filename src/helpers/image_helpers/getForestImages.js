f1a0d7af0d50639e5609b380647e3e7f00da8c77
nlanese21 <nlanese21@gmail.com>
2021-08-30 18:21:13 -0400

    Added all forest pathways, added walls object in the forest object inside of the AllImages object

--- src/helpers/image_helpers/getForestImages.js
+++ src/helpers/image_helpers/getForestImages.js
@@ -6,6 +6,12 @@ import open from 'src/images/forest/paths/open.jpg'
 import right_turn from 'src/images/forest/paths/right_turn.jpg'
 import straight from 'src/images/forest/paths/straight.jpg'
 import trees_back from 'src/images/forest/paths/trees_back.jpg'
+import Trees_front from 'src/images/forest/paths/Trees_front.jpg'
+import trees_full from 'src/images/forest/paths/trees_full.jpg'
+import trees_left from 'src/images/forest/paths/trees_left.jpg'
+import trees_right from 'src/images/forest/paths/trees_right.jpg'
+import left_turn_up from 'src/images/forest/paths/left_turn_up.jpg'
+import right_turn_up from 'src/images/forest/paths/right_turn_up.jpg'
 
 
 
@@ -20,4 +26,38 @@ export default class ForestImages{
     horizontal_path(){
         return (< img src={horizontal} />)
     }
+    tLeft_path(){
+        return (< img src={left_turn} />)
+    }
+    tLeftUp_path(){
+        return (< img src={left_turn_up} />)
+    }
+    open_path(){
+        return (< img src={open} /> )
+    }
+    tRight_path(){
+        return (< img src={right_turn} /> )
+    }
+    tRightUp_path(){
+        return (< img src={right_turn_up} /> )
+    }
+    forward_path(){
+        return (< img src={straight} /> )
+    }
+    trees_back(){
+        return (< img src={trees_back} />)
+    }
+    trees_front(){
+        return (< img src={Trees_front} />)
+    }
+    trees_full(){
+        return (< img src={trees_full} />)
+    }
+    trees_left(){
+        return (< img src={trees_left} />)
+    }
+    trees_right(){
+        return (< img src={trees_right} />)
+    }
+
 }
--- src/helpers/image_helpers/getImage.js
+++ src/helpers/image_helpers/getImage.js
@@ -1,2 +1,30 @@
 // Import all image files from the images folder
 // import '../images/<whatever>'
+import ForestImages from "./getForestImages";
+
+export const allImages = {
+    forest: {
+        pathways: {
+            fork: ForestImages.fork_path(),
+            fourway: ForestImages.fourway_path(),
+            horizonal: ForestImages.horizonal_path(),
+            turnLeft: ForestImages.tLeft_path(),
+            turnLeftUp: ForestImages.tLeftUp_path(),
+            open: ForestImages.open_path(),
+            turnRight: ForestImages.tRight_path(),
+            turnRightUp: ForestImages.tRightUp_path(),
+            forward: ForestImages.forward_path(),
+        },
+
+        walls: {
+            trees_back: ForestImages.trees_back(),
+        },
+        
+        enemeies: {
+
+        }
+    },
+    towns: {
+
+    },
+}
