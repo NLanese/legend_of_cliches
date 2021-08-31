78a55306485e401676f60ac34c263f898da6cfbf
nlanese21 <nlanese21@gmail.com>
2021-08-31 06:06:57 -0400

    Beginning map grid process. Added Reducers folder

---
+++ src/reducers/maps_reducers/forest_map_reducer.js
@@ -0,0 +1,38 @@
+import { allImages } from "../../helpers/image_helpers/getImage"
+const fullTrees = {img: allImages.forest.walls.trees_full, walkable: false, discovered: false, inv_item: "N/A", inv_efect: "N/A"}
+const leftTrees = {img: allImages.forest.walls.trees_left, walkable: false, discovered: false, inv_item: "N/A", inv_efect: "N/A"}
+const rightTrees = {img: allImages.forest.walls.trees_right, walkable: false, discovered: false, inv_item: "N/A", inv_efect: "N/A"}
+
+
+// This function will generate the 50x200 map 
+// Every tile will default to an object that looks like the following..
+
+    // {img: trees_full, walkable: false, discovered: false, inv_item: "N/A", inv_effect: "N/A"}
+    
+        // img will dictate what image is shown
+        // walkable determines if the player can go on this tile
+        // discovered determines if it will be revelaed when a player looks at the map
+        // inv_item will be N/A unless the space can be effected by an inventory item
+        // inv_effect will be the effect if the space is interacted by a player in possesion of inv_item
+function generateGrid(){
+    var mapGrid = new Array(50)
+    mapGrid.forEach( (element) => {
+        element = new Array(200)
+    })
+    mapGrid.forEach( (element) => {
+        element.forEach( (tile) => {
+            tile = fullTrees
+        })
+    })
+
+}
+
+
+export default function manageForestMap(
+    state= {
+        map_grid: generateGrid(),
+        enemies: 
+    }, 
+    action){
+
+    }
