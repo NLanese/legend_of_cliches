b79fa29b9776acf27b3f06449dffed4f4009028b
nlanese21 <nlanese21@gmail.com>
2021-10-30 04:10:46 -0400

    More Images added

--- src/helpers/Moves_And_Stats_Helpers/MoveTreeImgImporter.js
+++ src/helpers/Moves_And_Stats_Helpers/MoveTreeImgImporter.js
@@ -1,5 +1,9 @@
 import { allImages } from "../image_helpers/getImage";
 
+// This is exported to the MoveSelection as well as MovesList Components
 export default function MoveTreeImages(class_type){
-
+    if (class_type == "Mage"){
+        // Elemental, LifeEnergy, and Necromancer Tree Panels
+        return [allImages.moveTrees.mage_element, allImages.moveTrees.mage_life, allImages.moveTrees.mage_necro]
+    }
 }
